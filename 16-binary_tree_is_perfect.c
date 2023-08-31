1cf32c75820e60118f124e8b820e44beabac13c6
yasoug <ougouisyassine@gmail.com>
2023-08-31 12:10:39 -0700

    task 16 : commit 2

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,22 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - function that checks if a node is a leaf
- * @node: pointer to the node to check
- * Return: 1 if node is a leaf, otherwise 0
- */
-
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (node)
-	{
-		if (!node->left && !node->right)
-			return (1);
-	}
-	return (0);
-}
-
-/**
  * binary_tree_height - function that measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height
  * Return: the height of the tree, or 0 if tree is NULL
@@ -50,18 +34,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (binary_tree_is_leaf(tree))
-		return (1);
-
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
-	if (tree->left && tree->right)
-	{
-		if (left != right)
-			return (0);
-	}
-	if (left == 0 || right == 0)
-		return (0);
 
-	return (1);
+	if (left == right)
+		return (1);
+	return (0);
 }
