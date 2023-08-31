38533529a1421b9e7e52064400dd4ac8ab3f6ba7
yasoug <ougouisyassine@gmail.com>
2023-08-31 12:15:48 -0700

    task 16 : commit 3

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -37,7 +37,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
-	if (left == right)
+	if (left != right)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	return (0);
+
+	return (binary_tree_is_perfect(tree->left)
+		&& binary_tree_is_perfect(tree->right));
 }
